c26bcc06f224e31d61f0873424a093800e4f20a5
kostiantyn-oa <kostiantyn.ryzhykov@oa.edu.ua>
2025-09-16 17:36:33 +0300

    Updated

--- Lab-2/src/App.jsx
+++ Lab-2/src/App.jsx
@@ -6,63 +6,63 @@ const App = () => {
   const users = [
     {
       "id": 1,
-      "name": "Leanne Graham",
+      "name": "Mark Zuckerberg",
       "role": "Developer",
-      "avatarUrl": "https://www.svgrepo.com/download/1645/woman.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Zuckerberg-Jourov%C3%A1_meeting_%282020%29%28cropped_4%29.jpg/500px-Zuckerberg-Jourov%C3%A1_meeting_%282020%29%28cropped_4%29.jpg"
     },
     {
       "id": 2,
-      "name": "Ervin Howell",
+      "name": "Jessica Walsh",
       "role": "Designer",
-      "avatarUrl": "https://www.svgrepo.com/download/4559/man.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Jessica_Walsh_2013.jpg/500px-Jessica_Walsh_2013.jpg"
     },
     {
       "id": 3,
-      "name": "Clementine Bauch",
+      "name": "Evan Spiegel",
       "role": "Manager",
-      "avatarUrl": "https://www.svgrepo.com/download/18047/girl.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Evan_Spiegel%2C_founder_of_Snapchat.jpg/500px-Evan_Spiegel%2C_founder_of_Snapchat.jpg"
     },
     {
       "id": 4,
-      "name": "Patricia Lebsack",
+      "name": "Daniel Gross",
       "role": "Engineer",
-      "avatarUrl": "https://www.svgrepo.com/download/26728/woman.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/TechCrunch_Disrupt_San_Francisco_2018_-_day_2_%2843613758885%29.jpg/500px-TechCrunch_Disrupt_San_Francisco_2018_-_day_2_%2843613758885%29.jpg"
     },
     {
       "id": 5,
-      "name": "Chelsey Dietrich",
+      "name": "Cathy O'Neil",
       "role": "Analyst",
-      "avatarUrl": "https://www.svgrepo.com/download/40589/girl.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Cathy_O%27Neil_at_Google_Cambridge.jpg/500px-Cathy_O%27Neil_at_Google_Cambridge.jpg"
     },
     {
       "id": 6,
-      "name": "Mrs. Dennis Schulist",
+      "name": "Ernestine Fu",
       "role": "Coordinator",
-      "avatarUrl": "https://www.svgrepo.com/download/1645/woman.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Ernestine_Fu_%2820240513%29.jpg/500px-Ernestine_Fu_%2820240513%29.jpg"
     },
     {
       "id": 7,
-      "name": "Kurtis Weissnat",
+      "name": "Aaron Swartz",
       "role": "Developer",
-      "avatarUrl": "https://www.svgrepo.com/download/9283/man.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Aaron_Swartz_in_2011.jpg/500px-Aaron_Swartz_in_2011.jpg"
     },
     {
       "id": 8,
-      "name": "Nicholas Runolfsdottir V",
+      "name": "Julie Zhuo",
       "role": "Designer",
-      "avatarUrl": "https://www.svgrepo.com/download/27132/man.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Julie_Zhuo.jpg/500px-Julie_Zhuo.jpg"
     },
     {
       "id": 9,
-      "name": "Glenna Reichert",
+      "name": "Ruben Amorim",
       "role": "Manager",
-      "avatarUrl": "https://www.svgrepo.com/download/18047/girl.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/RubenAmorim4.png/375px-RubenAmorim4.png"
     },
     {
       "id": 10,
-      "name": "Clementina DuBuque",
+      "name": "Drew Houston",
       "role": "Engineer",
-      "avatarUrl": "https://www.svgrepo.com/download/26728/woman.svg"
+      "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Drew_Houston_at_Web_Summit_%28cropped%29.jpg/500px-Drew_Houston_at_Web_Summit_%28cropped%29.jpg"
     }
   ];
 
